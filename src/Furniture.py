1e9f908ee5fcc49b23e4b3a60bfa05da8345bddd
benaryorg <benedikt.bre3@gmail.com>
2014-02-18 16:35:22 +0100

    Added Furniture

---
+++ src/Furniture.py
@@ -0,0 +1,47 @@
+from Object import Object
+
+class Furniture(Object):
+	def __init__(self,mass,material):
+		Object.__init__(self,mass=mass)
+		self.setMaterial(material)
+
+	def __str__(self):
+		return ';'.join([self.material,Object.__str__(self)])
+
+	def setMaterial(self,material):
+		self.material=str(material)
+
+class Sittable(Furniture):
+	def __init__(self,mass,material,maxpersons,sitting=0):
+		Furniture.__init__(self,mass,material)
+		self.setMaxPersons(maxpersons)
+		self.setSitting(sitting)
+
+	def __str__(self):
+		return ';'.join([str(self.sitting),str(self.maxpersons)])
+
+	def setMaxPersons(self,persons):
+		persons=int(persons)
+		if persons>=0:
+			self.maxpersons=persons
+		else:
+			raise ValueError('Value cannot be negative!')
+
+	def setSitting(self,sitting):
+		sitting=int(sitting)
+		if sitting>=0:
+			if sitting<=self.maxpersons:
+				self.sitting=sitting
+			else:
+				raise ValueError('No space left!')
+		else:
+			raise ValueError('Value cannot be negative!')
+
+	def sitDown(self,count=1):
+		count=int(count)
+		self.setSitting(count+self.sitting)
+
+class Chair(Sittable):
+	def __init__(self,mass,material,maxpersons=1,sitting=0):
+		Sittable.__init__(self,mass,material,maxpersons,sitting)
+
